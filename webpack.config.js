d3c17f963a67d4aeeb73d83d10f5ddf006cba248
Raul F. de Melo <melo.raulf@gmail.com>
2017-03-14 23:14:34 +0000

    Create a webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const PRODUCTION = process.env.NODE_ENV === 'production'
+
+const entry = PRODUCTION
+  ? ['./src/index.js']
+  : ['./src/index.js', 'webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080']
+
+const plugins = PRODUCTION
+  ? []
+  : [new webpack.HotModuleReplacementPlugin()]
+
+module.exports = {
+  devtool: 'source-map',
+  entry: entry,
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: '/node_modules/'
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  }
+}
